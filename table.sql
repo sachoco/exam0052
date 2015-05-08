e7e8b6db2fcecc80909025c84395f5ff00e2f559
Satoshi Shiraishi <sachoco@gmail.com>
2015-05-08 18:12:21 +0200

    add sql command

---
+++ table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `entry` (
+`id` int(11) unsigned NOT NULL,
+  `link` varchar(100) NOT NULL,
+  `entryno` int(8) NOT NULL,
+  `title` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `date` int(8) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `entry`
+--
+ALTER TABLE `entry`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`), ADD UNIQUE KEY `link` (`link`);
+
+--
+-- AUTO_INCREMENT for table `entry`
+--
+ALTER TABLE `entry`
+MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
